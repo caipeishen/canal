dbd2cef1342453f2762bd86c43a6ac56d9c3fb30
Cai Peishen <peishen.cai@foxmail.com>
2022-01-05 16:13:25 +0800

    使用客户端接口canal增量数据

---
+++ src/test/java/cn/cps/CanalClientTest.java
@@ -0,0 +1,20 @@
+package cn.cps;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CanalClientTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
